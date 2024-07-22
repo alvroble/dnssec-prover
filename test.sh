3ff02359891e5f88673addde346a477acd16ec6c
Matt Corallo <git@bluematt.me>
2024-07-22 14:41:00 +0000

    Add a `RUST_VERSION` env argument to `test.sh` to enable MSRV tests

--- test.sh
+++ test.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -eox
 
-cargo test --no-default-features
-cargo test
-cargo test --no-default-features --features std
-cargo test --no-default-features --features tokio
-cargo test --no-default-features --features validation
-cargo test --features std,tokio,validation
-cargo test --no-default-features --features build_server
-cargo build --lib
-cargo build --lib --features std
-cargo build --lib --features tokio
-cargo build --lib --features validation
-cargo build --lib --features std,tokio,validation
-cargo build --lib --features std,tokio,validation --release
-cargo build --bin http_proof_gen --features build_server
-cargo doc --features std,tokio,validation
+cargo $RUST_VERSION test --no-default-features
+cargo $RUST_VERSION test
+cargo $RUST_VERSION test --no-default-features --features std
+cargo $RUST_VERSION test --no-default-features --features tokio
+cargo $RUST_VERSION test --no-default-features --features validation
+cargo $RUST_VERSION test --features std,tokio,validation
+cargo $RUST_VERSION test --no-default-features --features build_server
+cargo $RUST_VERSION build --lib
+cargo $RUST_VERSION build --lib --features std
+cargo $RUST_VERSION build --lib --features tokio
+cargo $RUST_VERSION build --lib --features validation
+cargo $RUST_VERSION build --lib --features std,tokio,validation
+cargo $RUST_VERSION build --lib --features std,tokio,validation --release
+cargo $RUST_VERSION build --bin http_proof_gen --features build_server
+cargo $RUST_VERSION doc --features std,tokio,validation
 cd fuzz
 RUSTFLAGS="--cfg=fuzzing" RUSTC_BOOTSTRAP=1 cargo build --features stdin_fuzz
 RUSTFLAGS="--cfg=fuzzing" RUSTC_BOOTSTRAP=1 cargo test
